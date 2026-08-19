95e50df72c9b08a8a987af0b71452236045a81b5
agenteguia <agenteguiaoficial@gmail.com>
2026-08-19 17:33:29 +0000

    feat: atualizar service worker e melhorar gerenciamento de cache

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,9 @@
 const CACHE = 'agente-guia-v1';
 const ASSETS = ['/', '/index.html', '/manifest.webmanifest'];
 self.addEventListener('install', (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS))));
-self.addEventListener('fetch', (event) => event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request))));
+self.addEventListener('activate', (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key))))));
+self.addEventListener('fetch', (event) => {
+	const requestUrl = new URL(event.request.url);
+	if (event.request.method !== 'GET' || requestUrl.origin !== self.location.origin) return;
+	event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request)));
+});
